fd6aefd83e04bcee752c10374055f1b18fc1bf96
Francisco2030 <Francisco2030@DESKTOP-5AP7NV0>
2026-01-29 18:40:31 -0500

    shooping

---
+++ Shooping/Shooping/Models/Data/Entities/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shooping.Models.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [Display (Name ="Pais")]
+        [MaxLength(50, ErrorMessage ="el campo {0} debe tener maximo {1} caracteres")]
+        [Required(ErrorMessage ="el campo {0} es obligatorio")]
+        public string Name { get; set; }
+
+
+    }
+}
---
+++ Shooping/Shooping/Models/Data/Entities/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace Shooping.Models.Data.Entities
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        //public DataContext(DbContextOptions<DataContext> options)
+        //    : base(options)
+        //{
+
+
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //crea un indice unico sobre el campo name
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+
+    }
+}
